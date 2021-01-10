6af204bcf181ac0a240a197ec32671c6de5f4fd3
deanbyrne95 <deanbyrne95+github@gmail.com>
2021-01-10 13:26:27 +0000

    Add files

---
+++ index.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const quotes = require("./quotes.json");
+
+const client = new Discord.Client();
+const keywords = ["Robert", "Bobby"];
+const activities_list = ["with Bessies Tits", "King of the Seven Kingdoms"];
+
+client.on("ready", () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
+        client.user.setActivity(activities_list[index]);
+    }, 10000);
+})
+
+client.on("message", function(message) { 
+    if (message.author.bot) return;
+    const fixedMessage = message.content.toLowerCase();
+    const triggered = keywords.some(word => fixedMessage.includes(word.toLowerCase()));
+    if (!triggered) {
+        console.log("NOT TRIGGERED!");
+        return;
+    }
+    
+    message.channel.send(get_random_quote());
+});
+
+function get_random_quote() {
+     // Returns random quote from quotes file
+     var random = quotes.quote[Math.floor(Math.random() * quotes.quote.length)];
+     console.log(random);
+     return random;
+}
+
+client.login(config.BOT_TOKEN);
